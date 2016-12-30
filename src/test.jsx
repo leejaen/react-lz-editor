227f243ef4675846ff6d960e33782b79f630a237
lizhen <leejaen@163.com>
2016-12-30 17:46:11 +0800

    Demo page

--- src/test.jsx
+++ src/test.jsx
@@ -5,7 +5,7 @@ class Test extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            content: "<p>Tips、Toast、弹窗、动作列表等等这几个词，虽然经常挂在嘴边，但是应该如何分类自己并不清楚，查阅大量资料后，我发现原来还有更多我不知道的知识点，每个人分类的维度也各不相同，有些按照模态、非模态弹窗来分类（Toast、Dialog、Actionbar 和 Snackbar）；有些按照弹窗、浮层来分等等，我觉得没有绝对的对错，只要能够有自己清晰的思考维度与参考依据，并且能够为实际工作带来指导意义，那就是有价值的，所以自己在经过筛选归类后，决定按照以下维度来定义我自己的弹窗体系，大纲如下（如有不妥之处，还请批评指正）：</p>"
+            content: "<p>根据山东省重污染天气应急工作小组办公室《关于启动重污染天气Ⅰ级响应的通知》，我市于12月29日20时发布重污染天气红色预警，并于12月30日20时采取包括机动车单双号限行等措施的重污染天气Ⅰ级应急响应。经气象部门与环保部门最新会商，由于天气形势发生了一定变化，12月31日至2017年1月1日，虽然空气扩散条件转差，31日仍以中度污染为主，预计1月1日空气质量以中度至重度为主，达不到严重污染的程度。为保障市民节假日出行，经市政府批准，暂停实施机动车单双号限行措施。其它应急响应措施继续执行。</p>"
         }
         this.receiveHtml = this.receiveHtml.bind(this);
     }
@@ -25,9 +25,8 @@ class Test extends React.Component {
             QINIU_DOMAIN_VIDEO_URL: "https://image.qiluyidian.mobi", //视频文件地址的前缀
             QINIU_DOMAIN_FILE_URL: "https://static.qiluyidian.com/", //其他文件地址前缀
         }
-        return <LzEditor active={true} HtmlContent={this.state.content} cbReceiver={this.receiveHtml} uploadConfig={uploadConfig} InlineStyle={false} Color={false} FullScreen={false}/>
+        return <LzEditor active={true} HtmlContent={this.state.content} cbReceiver={this.receiveHtml} uploadConfig={uploadConfig} Color={false} FullScreen={false}/>
     }
 }
 
-ReactDOM.render(
-    <Test/>, document.getElementById('hello'));
+ReactDOM.render(<Test/>, document.getElementById('test'));
