d2e96ecc25c1516b4b689b1858bf6b7301da0235
unknown <leejaen@gmail.com>
2017-02-22 15:40:17 +0800

    Fix reference path to support no webpack compile.

--- src/global/components/businessComponents.js
+++ src/global/components/businessComponents.js
@@ -2,6 +2,6 @@
  * Created by lizhen on 4/14/2016.
  */
 module.exports = {
-  UploadImage: require('businessComponents/UploadImage'),
-  GroupUpload:require('businessComponents/GroupUpload')
+  UploadImage: require('./businessComponents/UploadImage'),
+  GroupUpload:require('./businessComponents/GroupUpload')
 };
