1db5a895bb7402e2344b994449675ffd60feea27
unknown <leejaen@gmail.com>
2017-03-01 10:21:50 +0800

    Make file path much readability(/release to /publish).
    Remove /dist path
    
    Remove /dist path

--- release/main.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var _test = require('./test.jsx');
-
-var _test2 = _interopRequireDefault(_test);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
