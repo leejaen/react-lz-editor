4a46159c0a9b1391cd7f588546e74b85105aa522
unknown <leejaen@gmail.com>
2017-03-21 11:09:55 +0800

    :apple: Add title tip text of redo and undo.

--- src/editor/toolBar/undoredoControls.jsx
+++ src/editor/toolBar/undoredoControls.jsx
@@ -9,10 +9,10 @@ class UndoRedo extends Component {
     let className = 'RichEditor-styleButton';
     return (
       <div className="RichEditor-controls">
-        <span className='RichEditor-styleButton' onClick={()=>this.props.onToggle("undo")}>
+        <span className='RichEditor-styleButton' onClick={()=>this.props.onToggle("undo")} title="撤销（Ctrl-Z，Cmd-Z）">
           <Icon key="_undo" type="editor_undo" />
         </span>
-        <span className='RichEditor-styleButton' onClick={()=>this.props.onToggle("redo")}>
+        <span className='RichEditor-styleButton' onClick={()=>this.props.onToggle("redo")} title="重做（Ctrl-Y，Cmd-Shift-Z）">
           <Icon key="_redo" type="editor_redo" />
         </span>
       </div>
