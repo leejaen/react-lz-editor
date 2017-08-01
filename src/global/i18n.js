f4792c37475675c0e1be6479707bbbffed96aeb2
lizhen <leejaen@gmail.com>
2017-08-01 13:01:46 +0800

    Add english tranlation

--- src/global/i18n.js
+++ src/global/i18n.js
@@ -1,12 +1,21 @@
 module.exports = {
   lang: {
     'en': {
-      'wrongImageAddress': '图片地址错误！',
-      fullScreen:"full screen",
-      sourceCode:"source code"
+      fullScreen:"full",
+      quitFullScreen:"quit",
+      sourceCode:"source",
+      preview:"prev",
+      selectedText:"Please make sure you have selected text before add a new link!",
+      selectedLink:"Please make sure you have selected a link before remove it!",
+      successToDraftBox:"Context have been saved to Draft-Box",
+      successPasteCleanText:"Pasted no-style text success, it would not be display if the image url is 'Anti-Leech'.",
+      errorUploadingFile:"!!!!!!!!!!!!ERROR UPLOADING!!!!!!!!!!!!",
+      directToURL:"Please input url",
+      directToURLTip:"Please enter the regular URL(with \"http://\" or \"https://\" preffix.)",
+      markdownTip:"Please markdown context here",
+        wrongImageAddress: 'Wrong image address!'
     },
     'zh-CN': {
-      'wrongImageAddress': '图片地址错误！',
       fullScreen:"全屏",
       quitFullScreen:"退出全屏",
       sourceCode:"源码",
@@ -18,7 +27,8 @@ module.exports = {
       errorUploadingFile:"！！！！！！！！！！上传文件错误！！！！！！！！！！",
       directToURL:"请输出你要跳转的链接",
       directToURLTip:"请输入符合规范的网址链接（以“http://” 或 “https://”为前导）",
-      markdownTip:"请在这里编辑您的markdown内容"
+      markdownTip:"请在这里编辑您的markdown内容",
+      wrongImageAddress: '图片地址错误！'
     }
   }
 }
